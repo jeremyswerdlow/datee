71f8d073107ef0aa208104d122e5947896244723
Jeremy Swerdlow <jeremyjswerdlow@gmail.com>
2026-09-25 20:35:54 -0400

    Support runtime caching of images

--- public/sw.js
+++ public/sw.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./icons/icon-192.png",
-    "./icons/icon-512.png",
+    "./icon-192.png",
+    "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
@@ -20,9 +20,40 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+    const request = event.request;
+
+    if (request.method !== "GET") {
+        return;
+    }
+
+    const url = new URL(request.url);
+    const isImage =
+        url.origin === self.location.origin &&
+        /\.(png|jpg|jpeg|gif|webp|svg|avif)$/i.test(url.pathname);
+
+    if (!isImage) {
+        return;
+    }
+
     event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
+        caches.match(request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(request).then((networkResponse) => {
+                if (!networkResponse.ok) {
+                    return networkResponse;
+                }
+
+                const responseToCache = networkResponse.clone();
+
+                caches.open("datee-images-v1").then((cache) => {
+                    cache.put(request, responseToCache);
+                });
+
+                return networkResponse;
+            });
         }),
     );
 });
