82b930c71a38cef8a583b339c2d5cf172098745e
Jeremy Swerdlow <jeremyjswerdlow@gmail.com>
2026-09-25 20:18:20 -0400

    Take a first run at support for installing as a mobile app

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "datee-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)),
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        }),
+    );
+});
